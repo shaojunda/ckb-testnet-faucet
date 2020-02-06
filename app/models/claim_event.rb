7e8571d251eb52f21cc75cbc87850cd60c9b85a2
shaojunda <shaojunda@gmail.com>
2020-02-06 16:38:17 +0800

    chore: use enum on status

--- app/models/claim_event.rb
+++ app/models/claim_event.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
 class ClaimEvent < ApplicationRecord
+  enum status: { pending: 0, processed: 1 }
+  enum tx_status: { pending: 0, proposed: 1, committed: 2 }, _prefix: :tx
 end
 
 # == Schema Information
@@ -11,9 +15,9 @@ end
 #  created_at_unixtimestamp :integer
 #  fee                      :decimal(20, )
 #  ip_addr                  :inet
-#  status                   :integer          default(0)
+#  status                   :integer          default("pending")
 #  tx_hash                  :string
-#  tx_status                :integer          default(0)
+#  tx_status                :integer          default("pending")
 #  created_at               :datetime         not null
 #  updated_at               :datetime         not null
 #  ckb_transaction_id       :bigint
