4c79846f4846b5c8636e7dae9bf445fa0997611a
shaojunda <shaojunda@gmail.com>
2020-02-06 16:25:49 +0800

    chore: use anotate

--- app/models/claim_event.rb
+++ app/models/claim_event.rb
@@ -1,2 +1,20 @@
 class ClaimEvent < ApplicationRecord
 end
+
+# == Schema Information
+#
+# Table name: claim_events
+#
+#  id                       :bigint           not null, primary key
+#  address_hash             :string
+#  capacity                 :integer
+#  created_at_unixtimestamp :integer
+#  fee                      :decimal(20, )
+#  ip_addr                  :inet
+#  status                   :integer          default(0)
+#  tx_hash                  :string
+#  tx_status                :integer          default(0)
+#  created_at               :datetime         not null
+#  updated_at               :datetime         not null
+#  ckb_transaction_id       :bigint
+#
