4d77514f277d33ca01865a589bfb8ac2cdf99d88
shaojunda <shaojunda@gmail.com>
2020-02-07 17:20:51 +0800

    chore: set timezone to Beijing

--- config/application.rb
+++ config/application.rb
@@ -17,5 +17,6 @@ module CkbTestnetFaucet
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.time_zone = "Beijing"
   end
 end
