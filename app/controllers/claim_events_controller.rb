57571b86d5f1b6b05e41b31e167d5ee95b925f8e
shaojunda <shaojunda@gmail.com>
2020-02-07 15:57:52 +0800

    chore: save claim event

--- app/controllers/claim_events_controller.rb
+++ app/controllers/claim_events_controller.rb
@@ -1,2 +1,18 @@
+# frozen_string_literal: true
+
 class ClaimEventsController < ApplicationController
+  def create
+    claim_event = ClaimEvent.new(claim_events_params)
+    if claim_event.save
+      render json: claim_event
+    else
+      render json: claim_event.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def claim_events_params
+    params.require(:claim_event).permit(:address_hash)
+  end
 end
