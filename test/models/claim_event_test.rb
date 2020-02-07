568778f0d97448a66bb3d7c7e7522a75067a4ffb
shaojunda <shaojunda@gmail.com>
2020-02-07 19:58:18 +0800

    test: claim event validates

--- test/models/claim_event_test.rb
+++ test/models/claim_event_test.rb
@@ -1,7 +1,12 @@
-require 'test_helper'
+# frozen_string_literal: true
+
+require "test_helper"
 
 class ClaimEventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  context "validations" do
+    should validate_presence_of(:address_hash).on(:create)
+    should validate_presence_of(:capacity).on(:create)
+    should validate_presence_of(:created_at_unixtimestamp).on(:create)
+    should validate_presence_of(:ip_addr).on(:create)
+  end
 end
