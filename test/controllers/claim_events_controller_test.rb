673659252bb72228a8ea3a3a29c0a9892bdedc73
shaojunda <shaojunda@gmail.com>
2020-02-07 15:57:02 +0800

    test: should create new claim event

--- test/controllers/claim_events_controller_test.rb
+++ test/controllers/claim_events_controller_test.rb
@@ -1,7 +1,12 @@
-require 'test_helper'
+# frozen_string_literal: true
+
+require "test_helper"
 
 class ClaimEventsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create new claim event when address hash is valid" do
+    address_hash = "ckt1qyqd5eyygtdmwdr7ge736zw6z0ju6wsw7rssu8fcve"
+    assert_difference -> { ClaimEvent.count }, 1 do
+      post claim_events_url, params: { claim_event: { address_hash: address_hash } }
+    end
+  end
 end
