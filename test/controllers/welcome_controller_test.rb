c989ecb719d7674bbeaf08bb879d3479cf847e92
shaojunda <shaojunda@gmail.com>
2020-02-07 10:22:56 +0800

    test: homepage should render welcome component

---
+++ test/controllers/welcome_controller_test.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class WelcomeControllerTest < ActionDispatch::IntegrationTest
+  test "should get success when visit homepage" do
+     get root_url
+
+     assert_response :success
+  end
+
+  test "should render Welcome component" do
+    get root_url
+
+    assert_react_component "Welcome"
+  end
+end
