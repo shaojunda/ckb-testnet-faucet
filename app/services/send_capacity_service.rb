1daaf3c915f5e9fcb054a12f1b16be2afffeabd2
shaojunda <shaojunda@gmail.com>
2020-02-10 21:04:01 +0800

    refactor: abstract to  two split methods
    for state changes and send capacity logic

--- app/services/send_capacity_service.rb
+++ app/services/send_capacity_service.rb
@@ -12,23 +12,30 @@ class SendCapacityService
       if first_pending_event.tx_hash.present?
         tx = ckb_wallet.get_transaction(first_pending_event.tx_hash)
         if tx.present?
-          if tx.tx_status == "committed"
-            first_pending_event.processed!
-            first_pending_event.update!(tx_status: "committed")
-          else
-            first_pending_event.update!(tx_status: tx.tx_status)
-          end
+          handle_state_change(first_pending_event, tx)
         else
-          tx_hash = ckb_wallet.send_capacity(first_pending_event.address_hash, first_pending_event.capacity)
-          first_pending_event.update!(tx_hash: tx_hash, tx_status: "pending")
+          handle_send_capacity(first_pending_event)
         end
       else
-        tx_hash = ckb_wallet.send_capacity(first_pending_event.address_hash, first_pending_event.capacity)
-        first_pending_event.update!(tx_hash: tx_hash, tx_status: "pending")
+        handle_send_capacity(first_pending_event)
       end
     end
   end
 
   private
     attr_reader :ckb_wallet
+
+    def handle_state_change(first_pending_event, tx)
+      if tx.tx_status == "committed"
+        first_pending_event.processed!
+        first_pending_event.update!(tx_status: tx.tx_status)
+      else
+        first_pending_event.update!(tx_status: tx.tx_status)
+      end
+    end
+
+    def handle_send_capacity(first_pending_event)
+      tx_hash = ckb_wallet.send_capacity(first_pending_event.address_hash, first_pending_event.capacity)
+      first_pending_event.update!(tx_hash: tx_hash, tx_status: "pending")
+    end
 end
