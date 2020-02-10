5ea56926fa33c779ffb9061b0311bfd4fea7348a
shaojunda <shaojunda@gmail.com>
2020-02-10 20:53:38 +0800

    feat: change event and tx status
    according to different cases

--- app/services/send_capacity_service.rb
+++ app/services/send_capacity_service.rb
@@ -10,6 +10,18 @@ class SendCapacityService
       first_pending_event = ClaimEvent.order(:id).pending.first
       first_pending_event.lock!
       if first_pending_event.tx_hash.present?
+        tx = ckb_wallet.get_transaction(first_pending_event.tx_hash)
+        if tx.present?
+          if tx.tx_status == "committed"
+            first_pending_event.processed!
+            first_pending_event.update!(tx_status: "committed")
+          else
+            first_pending_event.update!(tx_status: tx.tx_status)
+          end
+        else
+          tx_hash = ckb_wallet.send_capacity(first_pending_event.address_hash, first_pending_event.capacity)
+          first_pending_event.update!(tx_hash: tx_hash, tx_status: "pending")
+        end
       else
         tx_hash = ckb_wallet.send_capacity(first_pending_event.address_hash, first_pending_event.capacity)
         first_pending_event.update!(tx_hash: tx_hash, tx_status: "pending")
