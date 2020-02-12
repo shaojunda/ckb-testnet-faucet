974792fc08788cd0e048da7914e1475452900ff9
shaojunda <shaojunda@gmail.com>
2020-02-13 01:07:56 +0800

    chore: responsive img

--- app/javascript/components/Header.tsx
+++ app/javascript/components/Header.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { Navbar } from "react-bootstrap";
-import Logo from "../images/ckb_logo.png";
 
 const Header: React.FC = () => {
   return (
     <Navbar className="nav-bg">
       <Navbar.Brand href="/">
-        <img alt="ckb logo" src={Logo} className="logo" /> React Bootstrap
+        <div className="nav-logo"></div>
       </Navbar.Brand>
     </Navbar>
   );
