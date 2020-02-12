c1a1b9ba1c900b5329b62d39261effe7fe4bf191
shaojunda <shaojunda@gmail.com>
2020-02-12 12:31:11 +0800

    feat: add simple footer

---
+++ app/javascript/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Navbar } from "react-bootstrap";
+
+const Footer: React.FC = () => {
+  const getCurrentYear = () => {
+    return new Date().getFullYear();
+  };
+  return (
+    <Navbar sticky="bottom" className="footer">
+      <p>{`Copyright © ${getCurrentYear()} Nervos Foundation. All Rights Reserved.`}</p>
+    </Navbar>
+  );
+};
+export default Footer;
