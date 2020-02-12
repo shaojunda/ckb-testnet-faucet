340efa4a0f0072e5c1b22b00ae5076796afa9022
shaojunda <shaojunda@gmail.com>
2020-02-13 01:08:25 +0800

    chore: add default transaction fee

--- app/models/claim_event.rb
+++ app/models/claim_event.rb
@@ -2,6 +2,7 @@
 
 class ClaimEvent < ApplicationRecord
   DEFAULT_CLAIM_CAPACITY = 5000 * 10**8
+  DEFAULT_TRANSACTION_FEE = 1000
   enum status: { pending: 0, processed: 1 }
   enum tx_status: { pending: 0, proposed: 1, committed: 2 }, _prefix: :tx
 
