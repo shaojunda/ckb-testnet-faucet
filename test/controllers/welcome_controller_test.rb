33b6540a60488bd4bd7e950ee130a7f1babb03eb
shaojunda <shaojunda@gmail.com>
2020-02-15 20:32:07 +0800

    test: component props

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -10,8 +10,16 @@ class WelcomeControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should render Welcome component" do
+    account = Account.official_account
+    official_account = { addressHash: account.address_hash, balance: account.ckb_balance.to_s }
+    aggron_explorer_host = Rails.application.credentials.AGGRON_EXPLORER_HOST
+
     get root_url
 
-    assert_react_component "Welcome"
+    assert_react_component "Welcome" do |props|
+      assert_empty props[:claimEvents][:data]
+      assert_equal official_account, props[:officialAccount]
+      assert_equal aggron_explorer_host, props[:aggronExplorerHost]
+    end
   end
 end
