6b14031027317fdd379bf9fffe4f3ecfd0d427ea
shaojunda <shaojunda@gmail.com>
2020-02-28 20:26:50 +0800

    chore: adjust tests

--- test/services/send_capacity_service_test.rb
+++ test/services/send_capacity_service_test.rb
@@ -9,7 +9,10 @@ class SendCapacityServiceTest < ActiveSupport::TestCase
     first_pending_event = ClaimEvent.order(:id).pending.first
     tx_hash = "0x1deb37a41c037919d8b0bbce6e7ac19fb00b7e12f0cacff369acd416369e72d9"
     ckb_wallet = mock
-    ckb_wallet.expects(:send_capacity).with(first_pending_event.address_hash, ClaimEvent::DEFAULT_CLAIM_CAPACITY, fee: ClaimEvent::DEFAULT_TRANSACTION_FEE, outputs_validator: "passthrough").returns(tx_hash)
+    tx = build_tx(tx_hash)
+    min_fee = tx.fee(1000) + 100
+    ckb_wallet.expects(:generate_tx).with(first_pending_event.address_hash, first_pending_event.capacity).returns(tx)
+    ckb_wallet.expects(:send_capacity).with(first_pending_event.address_hash, ClaimEvent::DEFAULT_CLAIM_CAPACITY, fee: min_fee, outputs_validator: "passthrough").returns(tx_hash)
 
     assert_changes -> { first_pending_event.reload.tx_hash }, from: nil, to: tx_hash do
       SendCapacityService.new(ckb_wallet).call
@@ -22,7 +25,10 @@ class SendCapacityServiceTest < ActiveSupport::TestCase
     first_pending_event = ClaimEvent.order(:id).pending.first
     tx_hash = "0x1deb37a41c037919d8b0bbce6e7ac19fb00b7e12f0cacff369acd416369e72d9"
     ckb_wallet = mock
-    ckb_wallet.expects(:send_capacity).with(first_pending_event.address_hash, ClaimEvent::DEFAULT_CLAIM_CAPACITY, fee: ClaimEvent::DEFAULT_TRANSACTION_FEE, outputs_validator: "passthrough").returns(tx_hash)
+    tx = build_tx(tx_hash)
+    min_fee = tx.fee(1000) + 100
+    ckb_wallet.expects(:generate_tx).with(first_pending_event.address_hash, first_pending_event.capacity).returns(tx)
+    ckb_wallet.expects(:send_capacity).with(first_pending_event.address_hash, ClaimEvent::DEFAULT_CLAIM_CAPACITY, fee: min_fee, outputs_validator: "passthrough").returns(tx_hash)
 
     SendCapacityService.new(ckb_wallet).call
 
@@ -81,7 +87,10 @@ class SendCapacityServiceTest < ActiveSupport::TestCase
     first_pending_event.update(tx_hash: tx_hash)
     ckb_wallet = mock
     ckb_wallet.expects(:get_transaction).with(tx_hash).returns(nil)
-    ckb_wallet.expects(:send_capacity).with(first_pending_event.address_hash, ClaimEvent::DEFAULT_CLAIM_CAPACITY, fee: ClaimEvent::DEFAULT_TRANSACTION_FEE, outputs_validator: "passthrough").returns(new_tx_hash)
+    tx = build_tx(new_tx_hash)
+    min_fee = tx.fee(1000) + 100
+    ckb_wallet.expects(:generate_tx).with(first_pending_event.address_hash, first_pending_event.capacity).returns(tx)
+    ckb_wallet.expects(:send_capacity).with(first_pending_event.address_hash, ClaimEvent::DEFAULT_CLAIM_CAPACITY, fee: min_fee, outputs_validator: "passthrough").returns(new_tx_hash)
 
     assert_changes -> { first_pending_event.reload.tx_hash }, from: tx_hash, to: new_tx_hash do
       SendCapacityService.new(ckb_wallet).call
@@ -97,9 +106,28 @@ class SendCapacityServiceTest < ActiveSupport::TestCase
     first_pending_event.update(tx_hash: tx_hash)
     ckb_wallet = mock
     ckb_wallet.expects(:get_transaction).with(tx_hash).returns(OpenStruct.new(tx_status: OpenStruct.new(status: "committed"), transaction: {}))
-
     assert_difference -> { Account.official_account.balance }, -first_pending_event.capacity do
       SendCapacityService.new(ckb_wallet).call
     end
   end
+
+  private
+    def build_tx(new_tx_hash)
+      out_point = CKB::Types::OutPoint.new(index: 0, tx_hash: "0xace5ea83c478bb866edf122ff862085789158f5cbff155b7bb5f13058555b708")
+      cell_deps = CKB::Types::CellDep.new(dep_type: "dep_group", out_point: out_point)
+      input = CKB::Types::Input.new(previous_output: CKB::Types::OutPoint.new(index: 0, tx_hash: "0x1b7af98007bf6128879798ac08fa26f863ded9aa3ebf1c04b321873b01042a21"), since: 0)
+      output = CKB::Types::Output.new(capacity: 500000000000, lock: CKB::Types::Script.new(args: "0x59a27ef3ba84f061517d13f42cf44ed020610061", code_hash: "0x9bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce8", hash_type: "type"), type: nil)
+      witness = CKB::Types::Witness.new(input_type: "", lock: "0x8702b06d59d6d0577ca55e6672bc6c21086e0b216d06e85605e965c82431f929082f222d5315139c246ad537ce77af0237a7de032f813504fc80a0b6e1327b2f00", output_type: "")
+      tx = CKB::Types::Transaction.new(cell_deps: [cell_deps], hash: new_tx_hash, header_deps: [], inputs: [input], outputs: [output], outputs_data: ["0x"], version: 0, witnesses: [witness])
+      tx.witnesses = tx.witnesses.map do |witness|
+        case witness
+        when CKB::Types::Witness
+          CKB::Serializers::WitnessArgsSerializer.from(witness).serialize
+        else
+          witness
+        end
+      end
+
+      tx
+    end
 end
